4090e7526bccae79133d5c6f78534aaed7b33e1b
Rodionsav <rodionsav@mate.academy>
2024-01-03 01:05:19 +0100

    clone react_phone-shop

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.scss';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Footer } from './Components/Footer/Footer';
+import { Header } from './Components/Header/Header';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+const App = () => {
+  return (
+    <body className="body">
+      <div className="App">
+        <Header />
+
+        <ScrollToTop />
+        <body className="body">
+          <Outlet />
+        </body>
+        <Footer />
+      </div>
+    </body>
+
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+import { Root } from './Components/Root';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
